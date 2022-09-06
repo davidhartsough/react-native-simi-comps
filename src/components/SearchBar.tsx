5bca655fc087c7013160e4215538a306bc156367
davidhartsough <hartsoughdavid@gmail.com>
2022-09-07 02:10:54 +1000

    wrap SearchBar in row View container

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -14,44 +14,46 @@ export default function SearchBar({
 }) {
   const clear = () => setQuery("");
   return (
-    <View row flexed margin={8}>
-      <View row style={{ position: "relative" }}>
-        <View
-          style={{
-            position: "absolute",
-            left: 10,
-            top: "50%",
-            transform: [{ translateY: -9 }],
-            zIndex: -1,
-          }}
-        >
-          <Icon size={18} name="search" />
-        </View>
-        <Input
-          style={{ paddingLeft: 36 }}
-          value={query}
-          onChangeText={setQuery}
-          placeholder="Search"
-        />
-      </View>
-      {query.length > 0 && (
-        <View
-          style={{
-            position: "absolute",
-            right: 10,
-            top: "50%",
-            transform: [{ translateY: -13 }],
-          }}
-        >
-          <IconButton
-            onPress={clear}
-            icon="x"
-            size={18}
-            bordered={false}
-            style={{ padding: 4 }}
+    <View row>
+      <View row flexed margin={8}>
+        <View row style={{ position: "relative" }}>
+          <View
+            style={{
+              position: "absolute",
+              left: 10,
+              top: "50%",
+              transform: [{ translateY: -9 }],
+              zIndex: -1,
+            }}
+          >
+            <Icon size={18} name="search" />
+          </View>
+          <Input
+            style={{ paddingLeft: 36 }}
+            value={query}
+            onChangeText={setQuery}
+            placeholder="Search"
           />
         </View>
-      )}
+        {query.length > 0 && (
+          <View
+            style={{
+              position: "absolute",
+              right: 10,
+              top: "50%",
+              transform: [{ translateY: -13 }],
+            }}
+          >
+            <IconButton
+              onPress={clear}
+              icon="x"
+              size={18}
+              bordered={false}
+              style={{ padding: 4 }}
+            />
+          </View>
+        )}
+      </View>
     </View>
   );
 }
